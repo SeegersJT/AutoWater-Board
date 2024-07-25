c7eac6745e85109e4c179c6c172ce2884fba41bb
Hanno Seegers <seegersjt@outlook.com>
2024-07-25 16:03:27 +0200

    Small Changes

--- src/devices.h
+++ src/devices.h
@@ -7,67 +7,66 @@
 // Base Device Class
 class Device {
 public:
+    int pin;
+    const char* name;
+
     Device(int pin, const char* name) : pin(pin), name(name) {}
 
     virtual void setup() = 0;
     virtual void updateState() = 0;
-
-    int pin;
-    const char* name;
 };
 
 // Relay Device Class
 class RelayDevice : public Device {
-public:
-    RelayDevice(int pin, const char* name) : Device(pin, name), state("OFF") {}
+    private:
+        const char* state;
 
-    void setup() override {
-        pinMode(pin, OUTPUT);
-        digitalWrite(pin, LOW); // Default state
-    }
+    public:
+        RelayDevice(int pin, const char* name) : Device(pin, name), state("OFF") {}
 
-    void updateState() override {
-        // No specific action for relays here; set state through methods
-    }
-
-    void setState(const char* newState) {
-        if (strcmp(state, newState) != 0) {
-            digitalWrite(pin, strcmp(newState, "ON") == 0 ? HIGH : LOW);
-            state = newState;
+        void setup() override {
+            pinMode(pin, OUTPUT);
+            digitalWrite(pin, LOW);
         }
-    }
 
-    const char* getState() const {
-        return state;
-    }
+        void updateState() override {
 
-private:
-    const char* state;
+        }
+
+        void setState(const char* newState) {
+            if (strcmp(state, newState) != 0) {
+                digitalWrite(pin, strcmp(newState, "ON") == 0 ? HIGH : LOW);
+                state = newState;
+            }
+        }
+
+        const char* getState() const {
+            return state;
+        }
 };
 
 // Moisture Meter Device Class
 class MoistureDevice : public Device {
-public:
-    MoistureDevice(int pin, const char* name) : Device(pin, name), moisturePercentage(0) {}
+    private:
+        int moisturePercentage;
+    public:
+        MoistureDevice(int pin, const char* name) : Device(pin, name), moisturePercentage(0) {}
 
-    void setup() override {
-        pinMode(pin, INPUT);
-    }
+        void setup() override {
+            pinMode(pin, INPUT);
+        }
 
-    void updateState() override {
-        int sensorValue = analogRead(pin);
-        moisturePercentage = map(sensorValue, MOISTURE_METERS_MIN, MOISTURE_METERS_MAX, 0, 100);
+        void updateState() override {
+            int sensorValue = analogRead(pin);
+            moisturePercentage = map(sensorValue, MOISTURE_METERS_MIN, MOISTURE_METERS_MAX, 0, 100);
 
-        if (moisturePercentage < 0) moisturePercentage = 0;
-        if (moisturePercentage > 100) moisturePercentage = 100;
-    }
+            if (moisturePercentage < 0) moisturePercentage = 0;
+            if (moisturePercentage > 100) moisturePercentage = 100;
+        }
 
-    int getMoisturePercentage() const {
-        return moisturePercentage;
-    }
-
-private:
-    int moisturePercentage;
+        int getMoisturePercentage() const {
+            return moisturePercentage;
+        }
 };
 
 #endif // DEVICES_H
